b57d52372512711d7ed60b6e2fa540c451c36b1d
Vladislav <tsyduev.b.1@gmailcom>
2021-04-04 17:59:21 +0500

    done first lab

--- first_lab/tests/test_first.py
+++ first_lab/tests/test_first.py
@@ -6,6 +6,8 @@ import numpy as np
 class Test_1_1(unittest.TestCase):
     def test_main(self):
         self.assertTrue(np.allclose(main([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), np.array([10, 8, 6, 4, 2])))
+        self.assertTrue(np.allclose(main([1, 2, 3, 4, 5, 6, 7, 8, 9]), np.array([8, 6, 4, 2])))
+        self.assertTrue(np.allclose(main([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]), np.array([2, 4, 6, 8, 10])))
 
 
 if __name__ == '__main__':
---
+++ first_lab/tests/test_second.py
@@ -0,0 +1,14 @@
+import unittest
+from first_lab.second_ex import main
+import numpy as np
+
+
+class Test_1_2(unittest.TestCase):
+    def test_main(self):
+        self.assertEqual(main("qwertyuiop",0,3), "qwe")
+        self.assertEqual(main("qwertyuiop",1,3), "wer")
+        self.assertEqual(main("qwertyuiop",1,4), "wert")
+
+
+if __name__ == '__main__':
+    unittest.main()
